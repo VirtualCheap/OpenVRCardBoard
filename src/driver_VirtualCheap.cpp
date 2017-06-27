d0ffa5d47edd6619fb287567ebc022c3d426716d
ParkerMc <k5npr@yahoo.com>
2017-06-27 12:52:14 -0500

    Moved watchdog to it's own file

--- src/driver_VirtualCheap.cpp
+++ src/driver_VirtualCheap.cpp
@@ -6,6 +6,8 @@
 #include <string.h>
 #include <thread>
 
+#include "watchdogdriver_virtualcheap.h"
+
 #if defined( _WINDOWS )
 #include <Windows.h>
 #endif
@@ -63,84 +65,7 @@ static const char * const k_pch_Sample_RenderHeight_Int32 = "renderHeight";
 static const char * const k_pch_Sample_SecondsFromVsyncToPhotons_Float = "secondsFromVsyncToPhotons";
 static const char * const k_pch_Sample_DisplayFrequency_Float = "displayFrequency";
 
-//-----------------------------------------------------------------------------
-// Purpose:
-//-----------------------------------------------------------------------------
-
-class WatchdogDriver_VirtualCheap : public IVRWatchdogProvider
-{
-public:
-    WatchdogDriver_VirtualCheap();
-
-	virtual EVRInitError Init( vr::IVRDriverContext *pDriverContext ) ;
-	virtual void Cleanup() ;
-
-private:
-	std::thread *m_pWatchdogThread;
-};
-
-WatchdogDriver_VirtualCheap::WatchdogDriver_VirtualCheap()
-{
-    m_pWatchdogThread = nullptr;
-}
-
 WatchdogDriver_VirtualCheap g_watchdogDriverNull;
-
-
-bool g_bExiting = false;
-
-void WatchdogThreadFunction(  )
-{
-	while ( !g_bExiting )
-	{
-#if defined( _WINDOWS )
-		// on windows send the event when the Y key is pressed.
-		if ( (0x01 & GetAsyncKeyState( 'Y' )) != 0 )
-		{
-			// Y key was pressed. 
-			vr::VRWatchdogHost()->WatchdogWakeUp();
-		}
-		std::this_thread::sleep_for( std::chrono::microseconds( 500 ) );
-#else
-		// for the other platforms, just send one every five seconds
-		std::this_thread::sleep_for( std::chrono::seconds( 5 ) );
-		vr::VRWatchdogHost()->WatchdogWakeUp();
-#endif
-	}
-}
-
-EVRInitError WatchdogDriver_VirtualCheap::Init( vr::IVRDriverContext *pDriverContext )
-{
-	VR_INIT_WATCHDOG_DRIVER_CONTEXT( pDriverContext );
-	InitDriverLog( vr::VRDriverLog() );
-
-	// Watchdog mode on Windows starts a thread that listens for the 'Y' key on the keyboard to 
-	// be pressed. A real driver should wait for a system button event or something else from the 
-	// the hardware that signals that the VR system should start up.
-	g_bExiting = false;
-	m_pWatchdogThread = new std::thread( WatchdogThreadFunction );
-	if ( !m_pWatchdogThread )
-	{
-		DriverLog( "Unable to create watchdog thread\n");
-		return VRInitError_Driver_Failed;
-	}
-
-	return VRInitError_None;
-}
-
-
-void WatchdogDriver_VirtualCheap::Cleanup()
-{
-	g_bExiting = true;
-	if ( m_pWatchdogThread )
-	{
-		m_pWatchdogThread->join();
-		delete m_pWatchdogThread;
-		m_pWatchdogThread = nullptr;
-	}
-
-	CleanupDriverLog();
-}
 
 
 //-----------------------------------------------------------------------------
